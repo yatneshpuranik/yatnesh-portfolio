70c8fa08454b2206a365e7436d17f24b43f6a61f
Yatnesh Puranik <yatneshpuranik@gmail.com>
2026-07-08 22:48:06 +0530

    x

--- server/server.js
+++ server/server.js
@@ -11,7 +11,9 @@ connectDB();
 
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
-
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
 const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
 });
